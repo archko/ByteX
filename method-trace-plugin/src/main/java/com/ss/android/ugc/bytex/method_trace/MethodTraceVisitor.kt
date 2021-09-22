c3aa897bdacc2bbb17eb0a21a6ed338792705eba
archko <archko@gmail.com>
2021-09-22 22:10:58 +0800

    trace method start and end

--- method-trace-plugin/src/main/java/com/ss/android/ugc/bytex/method_trace/MethodTraceVisitor.kt
+++ method-trace-plugin/src/main/java/com/ss/android/ugc/bytex/method_trace/MethodTraceVisitor.kt
@@ -2,6 +2,7 @@ package com.ss.android.ugc.bytex.method_trace
 
 import com.ss.android.ugc.bytex.common.Constants
 import com.ss.android.ugc.bytex.common.visitor.BaseClassVisitor
+import org.objectweb.asm.Label
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
@@ -37,7 +38,7 @@ class MethodTraceVisitor(
         mv = super.visitMethod(access, name, desc, signature, exceptions)
 
         if ("hello" == name || "hello2" == name) {
-            val sb = StringBuilder();
+            /*val sb = StringBuilder();
             sb.append("name:").append(name)
                 .append(" desc;").append(desc);
             mv.visitFieldInsn(
@@ -54,7 +55,8 @@ class MethodTraceVisitor(
                 "(Ljava/lang/String;)V",
                 false
             );
-            context.logger.i("MethodTraceVisitor", "----插桩----className: $desc  methodName: ${name}------")
+            context.logger.i("MethodTraceVisitor", "----插桩----className: $desc  methodName: ${name}------")*/
+            return InlineMethodTraceVisitor(context, mv, name, desc)
         }
 
         return mv
@@ -63,7 +65,6 @@ class MethodTraceVisitor(
     private class InlineMethodTraceVisitor internal constructor(
         private val context: MethodTraceContext,
         private val methodVisitor: MethodVisitor,
-        private val owner: String,
         private val name: String,
         private val desc: String
     ) :
@@ -98,31 +99,69 @@ class MethodTraceVisitor(
                 "(Ljava/lang/String;)V",
                 false
             )
-            methodVisitor.visitMaxs(2, 2)
         }
 
-        override fun visitCode() {
+        /*override fun visitCode() {
             super.visitCode()
-            methodVisitor.visitMethodInsn(
-                Opcodes.INVOKEVIRTUAL,
-                "java/io/PrintStream",
-                "println",
-                "(Ljava/lang/String;)V",
-                false
-            )
+
             context.logger.i("TraceMethodVisitor", "start methodName: ${name}------")
         }
 
         override fun visitInsn(opcode: Int) {
             if (opcode == Opcodes.RETURN) {
+                context.logger.i("TraceMethodVisitor", "end methodName: ${name}------")
+            }
+            super.visitInsn(opcode)
+        }*/
+        lateinit var l0: Label
+        override fun visitCode() {
+            super.visitCode()
+            l0 = Label()
+            methodVisitor.visitLabel(l0)
+            methodVisitor.visitLineNumber(14, l0)
+            methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false)
+            methodVisitor.visitVarInsn(Opcodes.LSTORE, 1)
+        }
+
+        override fun visitInsn(opcode: Int) {
+            if (opcode == Opcodes.RETURN) {
+                context.logger.i("TraceMethodVisitor", "end methodName: ${name}------")
+
+                val l2 = Label()
+                methodVisitor.visitLabel(l2)
+                methodVisitor.visitLineNumber(16, l2)
+                methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false)
+                methodVisitor.visitVarInsn(Opcodes.LSTORE, 3)
+                val l3 = Label()
+                methodVisitor.visitLabel(l3)
+                methodVisitor.visitLineNumber(17, l3)
+                methodVisitor.visitLdcInsn("time:")
+                methodVisitor.visitVarInsn(Opcodes.LLOAD, 3)
+                methodVisitor.visitVarInsn(Opcodes.LLOAD, 1)
+                methodVisitor.visitInsn(Opcodes.LSUB)
+                methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Long", "valueOf", "(J)Ljava/lang/Long;", false)
                 methodVisitor.visitMethodInsn(
-                    Opcodes.INVOKEVIRTUAL,
-                    "java/io/PrintStream",
-                    "println",
-                    "(Ljava/lang/String;)V",
+                    Opcodes.INVOKESTATIC,
+                    "kotlin/jvm/internal/Intrinsics",
+                    "stringPlus",
+                    "(Ljava/lang/String;Ljava/lang/Object;)Ljava/lang/String;",
                     false
                 )
-                context.logger.i("TraceMethodVisitor", "end methodName: ${name}------")
+                methodVisitor.visitVarInsn(Opcodes.ASTORE, 5)
+                methodVisitor.visitInsn(Opcodes.ICONST_0)
+                methodVisitor.visitVarInsn(Opcodes.ISTORE, 6)
+                methodVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
+                methodVisitor.visitVarInsn(Opcodes.ALOAD, 5)
+                methodVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V", false)
+                val l4 = Label()
+                methodVisitor.visitLabel(l4)
+                methodVisitor.visitLineNumber(18, l4)
+                methodVisitor.visitInsn(Opcodes.RETURN)
+                val l5 = Label()
+                methodVisitor.visitLabel(l5)
+                methodVisitor.visitLocalVariable("start", "J", null, l0, l5, 1)
+                mv.visitLocalVariable("end", "J", null, l3, l5, 3)
+                //mv.visitLocalVariable("this", "Lasm/MyMethodClassVisitor;", null, l0, l5, 0)
             }
             super.visitInsn(opcode)
         }
